aef979f110ec09bdb242255faeb3c78d951992c0
paysimple-io <support@tapcube.co>
2026-06-08 12:42:15 -0400

    Update create-checkout.js

--- api/create-checkout.js
+++ api/create-checkout.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
       });
     }
 
-  const qualifiesForPromo = childQty >= 65;
+  const qualifiesForPromo = childQty >= 60;
 
 const session = await stripe.checkout.sessions.create({
   mode: childQty > 0 ? 'subscription' : 'payment',
